ef022e684f5bc9081e949b8b81b236c26f8785ad
whoami1711 <74840563+whoami1711@users.noreply.github.com>
2025-03-31 02:38:36 +0700

    Update calculator.py

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -1,1 +1,16 @@
+from .basic import add, subtract
+from .advanced import multiply, divide
+
+class Calculator:
+    def add(self, a, b):
+        return add(a, b)
+
+    def subtract(self, a, b):
+        return subtract(a, b)
+
+    def multiply(self, a, b):
+        return multiply(a, b)
+
+    def divide(self, a, b):
+        return divide(a, b)
 
